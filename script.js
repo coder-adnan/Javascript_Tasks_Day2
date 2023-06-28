1e82989446e1acff15649e0c3947e7ca328c4bd4
Adnan <adnanmaneri2015@gmail.com>
2023-06-28 09:26:44 +0500

    final commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Problem Statement:
+// Task 1: Reverse a String
+// Write a function called reverseString that takes a string as input and returns the reversed version of the string. For example, if the input is "hello", the function should return "olleh".
+const reverseString = str => {
+  //   return str.split("").reverse(str).join("");
+  //   return [...str].reverse().join("");
+  //   return Array.from(str).reverse().join("");
+};
+
+console.log(reverseString("Hello"));
